450ccee09db22d4b3333f122d846abbc23444151
imelda-kr <ik092034@oss.unist.hr>
2024-11-11 10:57:00 +0100

    Update 03_ik_03.cpp

--- 03_ik_03.cpp
+++ 03_ik_03.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+
+int main(){
+    std::vector<int> vek {5,6,4,2,3};
+    std::sort(vek.begin(),vek.end());
+    std::vector<int>::iterator manji=std::min_element(vek.begin(),vek.end());
+    vek.insert(manji,0);
+    int suma=0;
+    for(int i=0;i<vek.size();i++){
+        suma+=vek[i];
+     }
+     std::vector<int>::iterator najveci = std::max_element(vek.begin(), vek.end());
+     vek.insert(najveci+1,suma);
+     for(int i=0;i<vek.size();i++){
+         int n=vek[i];
+        std::cout<<n<<" ";
+     }
+
+    return 0;
+
+}
 
